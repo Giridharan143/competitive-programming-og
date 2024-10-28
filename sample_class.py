7a1f8c095f68c5c8883403e4ccd48cca8a960ad0
Giridharan K <krishnamoorthygiri143@gmail.com>
2024-10-28 20:48:28 +0530

    newly updated program sample_class contains my learnig curve in class in python and learned that using self is important in method classification

---
+++ sample_class.py
@@ -0,0 +1,18 @@
+print ("hello\nnice to meet you\n")
+class name:
+    def uppercase(self,text):
+        txt = text.upper()
+        return txt
+
+    def lowercase(self,text):
+        txt=text.lower()
+        return txt
+
+namer = name()
+input_name = input("")
+print("If you want to change your case to upper give 0 and if you want to change your case to lower give input as 1")
+case=int(input(""))
+if(case==0):
+    print(namer.uppercase(input_name))
+if(case==1):
+    print(namer.lowercase(input_name))
