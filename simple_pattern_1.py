024a79f936d05cdd54073bdb0d69299921a83ad8
Giridharan K <krishnamoorthygiri143@gmail.com>
2024-10-29 08:55:32 +0530

    nothing

--- simple_pattern_1.py
+++ simple_pattern_1.py
@@ -94,8 +94,43 @@ for i in range(1,n+1):
 '''
 '''
 n = 5
-for i in range(1,n+1):
-    for j in range(i,0,-1):
+for i in range(0,n+1):
+    for j in range(i,-1,-1):
         print('*', end=' ')
     print()
+#print("\n")
+for i in range(n,-1,-1):
+    for j in range(i,-1,-1):
+        print('*', end=" ")
+    print()
+
+#output :-
+* 
+* *
+* * *
+* * * *
+* * * * *
+* * * * * *
+* * * * * *
+* * * * *
+* * * *
+* * *
+* *
+*
 '''
+n=5
+
+for i in range(1,n+1):
+    print(' '*(n-i),end='')
+    for j in range(i):
+        print('*',end='')
+    print()
+
+
+
+
+#Lower Pattern
+for i in range(n,-1,-1):
+    for j in range(i,-1,-1):
+        print('*',end=" ")
+    print()
