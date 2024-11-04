dce221e2564ccb2ab40ac74a4dacf44eb00421dd
Giridharan K <krishnamoorthygiri143@gmail.com>
2024-11-05 00:36:53 +0530

    All the problems in the Beginners math problem is said to be covered and pushed in the repo

--- Helpsheet/Beginner_math_prob/print_all_divisores_of_number_cd_ninja.py
+++ Helpsheet/Beginner_math_prob/print_all_divisores_of_number_cd_ninja.py
@@ -5,3 +5,11 @@ Sample Output 1 :
 1 2 5 10
 
 '''
+def divisors(n):
+    out=[]
+    for idx in range(1,n+1):
+        if n%idx==0:
+            out.append(idx)
+    return out
+
+print(divisors(10))
