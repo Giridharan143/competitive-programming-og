0350f002e7bf7498990348a8f03130c607f505c5
Giridharan K <krishnamoorthygiri143@gmail.com>
2024-11-18 21:33:07 +0530

    new 31st problem uploaded in the git

--- Helpsheet/Arrays/Two Pointer/remove_duplicates_from_sorted_array_gfg.py
+++ Helpsheet/Arrays/Two Pointer/remove_duplicates_from_sorted_array_gfg.py
@@ -1,15 +1,16 @@
 def rem_dupli(nums):
-    out=[];flag=False
-    for idx in range(0,len(nums)):
-        ldx=len(nums)-1
-        if nums[idx]==nums[ldx]:
-            out.append(nums[idx])
-            nums.remove(nums[idx])
-            ldx-=1
-        if len(nums)==0:
-            return out
-    return out
+    if not nums and len(nums)==1:
+        return len(nums)
+    # Vigiilante in shadows - Method to solve the problem in Array
+    vigilant=0
+    for idx in range(1,len(nums)):
+        if nums[idx]!=nums[vigilant]:
+            vigilant+=1
+            nums[vigilant]=nums[idx]
+    print(nums)
 
 
-a=[2, 2, 2, 2, 2]
+    return vigilant+1
+
+a=[2, 5, 2, 2, 2]
 print(rem_dupli(a))
