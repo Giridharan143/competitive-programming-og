d6a0642073b4fcfa1fd931797eb65f007a3d3c44
Giridharan K <krishnamoorthygiri143@gmail.com>
2024-12-10 13:17:21 +0530

    Succesfully completed the Restaurant Mangament system and It was so Fuckinnnn Hectic to complete because of the addition of list and using it an reference was superhard actually and chatgpt is dumb for sure peace

--- long programming/java_implementation/Restaurent Management System/Restaurant.java
+++ long programming/java_implementation/Restaurent Management System/Restaurant.java
@@ -1,3 +1,42 @@
+import java.util.Scanner;
+
+import Waiter.*;
+
 public class Restaurant{
-    
+    //static Order ord;
+            public static void main(String[] args) {
+                Scanner inp = new Scanner(System.in);
+                Order ord = new Order();
+        while (true) {
+            System.out.println("Select an option:");
+            System.out.println("1. Add Menu Item");
+            System.out.println("2. View All Menu Items");
+            System.out.println("3. Place Order");
+            System.out.println("4. View All Orders");
+            System.out.println("5. Exit");
+            int choice = inp.nextInt();
+            inp.nextLine();  // Consume newline character
+
+            switch (choice) {
+                case 1:
+                    ord.addMenuItem();
+                    break;
+                case 2:
+                    ord.viewAllItems();
+                    break;
+                case 3:
+                    ord.placeOrder();
+                    break;
+                case 4:
+                    ord.viewOrders();
+                    break;
+                case 5:
+                    System.out.println("Exiting the system.");
+                    inp.close();
+                    return; // Exit the program
+                default:
+                    System.out.println("Invalid choice, please try again.");
+            }
+        }
+    }
 }
--- long programming/java_implementation/Restaurent Management System/Waiter/Order.java
+++ long programming/java_implementation/Restaurent Management System/Waiter/Order.java
@@ -3,19 +3,24 @@ package Waiter;
 import java.util.*;
 
 public class Order {
-    private Menu men;
+   // private Menu men;
     int orderId;
     double totalPrice;
-    Boolean served=true;
+    Boolean served=false;    
     //Menu m = new Menu();
-    ArrayList<Double> items = new ArrayList<Double>();
-    ArrayList<Menu> menus = new ArrayList<Menu>();
-    ArrayList<Order> orders = new ArrayList<Order>();
+    //ArrayList<Double> items = new ArrayList<Double>();
+    //ArrayList<Menu> orderitems = new ArrayList<>(); // this contains the items which are present in the items
+    ArrayList<Menu> items = new ArrayList<>(); // this contains the addition of all the elements which are selected by the user
+    ArrayList<Menu> menus = new ArrayList<Menu>(); // this contains the list of all the part of the menus available for it
+    ArrayList<Order> orders = new ArrayList<Order>(); // this contains the pointer to the addresss of the required node is arrived
     Scanner inp = new Scanner(System.in);
 public Order(int a){ // Creating an Constructor
         this.orderId=a;
        //this.men=c;    
     }
+public Order(){
+    // to initialise an empty constructor
+}
 
 public int getOrderId(){
     return this.orderId;
@@ -31,6 +36,12 @@ public Boolean getServablitiy(){
 public void setServable(Boolean a){
     this.served=a;
 }
+/*
+public void addMenuItem(Menu item){
+    this.orderitems.add(item);
+    this.totalPrice+=item.getPrice();
+}
+    */
 
 public void addMenuItem(){
     System.out.println("Kindly Enter the Item Id: ");
@@ -44,12 +55,13 @@ public void addMenuItem(){
     
     System.out.println("The Menu Has Been Added Successfully !!!");
 }
-
+/*
 
 public void getMenu(){
     Menu m = new Menu();
     return m;
 }
+    */
 public void viewAllItems(){
     if(menus.isEmpty()){
         System.out.println("There are no Items in the Menu, Kindly Update it !!");        
@@ -63,6 +75,54 @@ public void viewAllItems(){
 }
 
 public void placeOrder(){
+
+    System.out.println("Kindly enter the Order ID : ");
+    int orderId = inp.nextInt();
+    inp.nextLine();
+    //ArrayList<Menu> items = new ArrayList<>();
+    while(true){
+    System.out.println("Enter the Item Id which needs to be ordered || If done press -8");
+    int itemi = inp.nextInt();
+    inp.nextLine();
+    if(itemi==-8)
+        break;
+    Menu selectedItem =null;
+    
+    for(Menu idx: menus){
+        if(idx.getItemId()==itemi){
+            selectedItem=idx;
+        }
+    }
+    if(selectedItem!=null){
+        this.items.add(selectedItem);
+        this.totalPrice+=selectedItem.getPrice();
+        System.out.println("The Food : "+selectedItem.getName()+" is said to be ordered with price of "+selectedItem.getPrice());
+    }
+    else{
+        System.out.println("The seleted item is not available in the menu");
+    }
+    }
+
+  // Creating an reference for getting the new Order 
+
+  Order newOrder = new Order();
+  newOrder.items=this.items;
+  newOrder.totalPrice=this.totalPrice;
+  orders.add(newOrder);
+  System.out.println("The order "+orderId+" is done successfully");
+  newOrder.setServable(true);
+}
+public void viewOrders(){
+    for (Menu idx:items){
+        System.out.println(idx.getName());
+    }
+    for (Order idx:orders){
+        System.out.println(idx.getTotalPrice());
+    }
+}
+}
+/*
+public void placeOrder(){
     
     System.out.println("Kindly Enter the Item ID: || To Exit press -2");
     int itemIdi = inp.nextInt(); 
@@ -71,11 +131,12 @@ public void placeOrder(){
     inp.nextLine();
     for (Menu idx : menus){
         if(idx.getItemId()==itemIDi){
-            orders.add(new Order(orderIdi);
+            orders.add(new Order(orderIdi));
         }
     }
 
 }
+    
 
 public void viewAllOrders(){
     for(Order idx : orders){
@@ -85,7 +146,7 @@ public void viewAllOrders(){
     }
 }
 
-
+*/
 
 
 
