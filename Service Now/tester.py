6aabca6e1f936be54cd5e2811007753f06310578
Giridharan K <krishnamoorthygiri143@gmail.com>
2025-01-31 15:19:29 +0530

    Added the repo for service now n my repo please make sure to use it for further purposes

---
+++ Service Now/tester.py
@@ -0,0 +1,10 @@
+
+def first_pattern():
+    for idx in range(1,6):
+        print(" "*(6-comp),end=" ")  
+        print("* "*idx,end=" ")
+        print()
+        comp+=1
+
+def second_pattern():
+    
