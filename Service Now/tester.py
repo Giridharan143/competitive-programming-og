d57e16ae611f61ecd0d94d699f610cc4cf023a4b
Giridharan K <114174558+Giridharan143@users.noreply.github.com>
2025-01-31 15:21:52 +0530

    changes are done

--- Service Now/tester.py
+++ Service Now/tester.py
@@ -1,10 +1,9 @@
 
 def first_pattern():
+    comp=0
     for idx in range(1,6):
         print(" "*(6-comp),end=" ")  
         print("* "*idx,end=" ")
         print()
         comp+=1
 
-def second_pattern():
-    
