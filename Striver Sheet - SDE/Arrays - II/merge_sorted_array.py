a41106f36997a1198edfe2fa1102f9371132c8b6
Giridharan K <krishnamoorthygiri143@gmail.com>
2025-02-17 12:26:17 +0530

    latest

--- Striver Sheet - SDE/Arrays - II/merge_sorted_array.py
+++ Striver Sheet - SDE/Arrays - II/merge_sorted_array.py
@@ -3,6 +3,46 @@ class main:
         
         
         return
+    def binaryRec(self,nums,x):
+            mid=int (len(nums)/2) 
+            if not nums:
+                print(f"Number {x} is not Found :( ")           
+                return
+            if nums[mid]==x:
+                print(f"Number {x} is found :)")
+                return
+            elif nums[mid]>x:
+                self.binaryRec(nums[:mid],x)
+            else:
+                self.binaryRec(nums[mid+1:],x)
+            return
+    """
+    # def binarySearchingTest(target):
+    #     nums=[1,4,6,2,3,7,8,9,10]
+    #     nums.sort()
+    #     binaryRec(nums,target)
+    
+    # binarySearchingTest(7)
+    """
+
+    def sampleApproach():
+        nums=[1,2,3,4,5]
+        first=0
+        last=len(nums)-1
+        print("Before Sorting -> ",nums)
+        while(first!=last):
+            nums[first],nums[last]=nums[last],nums[first]
+            first+=1
+            last-=1
+        print("After Sorting ->  ",nums)
+        return
+    
 """
 using these line for commiting my code for future doubts !!!
 """
+
+    
+# nums=[1,4,6,2,3,7,8,9,10]
+# nums.sort()
+# s=main()
+# s.binaryRec(nums,8)
--- Striver Sheet - SDE/Arrays - II/tempCodeRunnerFile.py
+++ Striver Sheet - SDE/Arrays - II/tempCodeRunnerFile.py
@@ -1,2 +1,4 @@
-print(merged)
-        return
+nums=[1,4,6,2,3,7,8,9,10]
+nums.sort()
+s=main()
+s.binaryRec(nums,8)
