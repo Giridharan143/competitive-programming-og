7dfa0b98e99ac8d619bff65c5740c8486fe76154
Giridharan143 <krishnamoorthygiri143@gmail.com>
2025-04-15 22:54:40 +0530

    casual comeback commit maamey!!!

---
+++ Zoho/dotnet/TrainingSolution1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TrainingSolution1
+{
+    class Sample1
+    {
+        public static int recursion(int num)
+        {
+            if (num == 1)
+                return 1;
+            return num + recursion(num - 1);
+        }
+
+        public static string word(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return "";
+
+            string tempi = str[0].ToString();
+            return word(str.Substring(1)) + tempi;
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("The Total sum of 5: {0}",recursion(5));
+            Console.WriteLine("Enter the Word: ");
+            string? temp = Console.ReadLine();
+            Console.WriteLine("The Reverse of the word is {0}: {1}",temp,word(temp));
+        }
+    }
+}
