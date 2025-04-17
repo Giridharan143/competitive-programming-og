1c52722be6a0100b3800729e1f36855fbf6ca415
Giridharan143 <krishnamoorthygiri143@gmail.com>
2025-04-17 23:30:19 +0530

    changes are temp given - for dump

---
+++ Using_Pambu/SamplePyFile1/SamplePyFile1.py
@@ -0,0 +1,12 @@
+def funcCaller(a):
+    return a*a
+def funcCaller(a):
+    return a+"lo"
+def funcCaller(b):
+    return b+"li"
+#print("giri ")
+try:
+    print(funcCaller("giri"))
+    print(funcCaller("giri"))
+except Exception as e :
+    print("Error: ",e)
